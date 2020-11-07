584852cd92994ad1a78b5c7b70bb2b2fd0367876
Pradip222 <pvp9765@gmail.com>
2020-11-07 15:20:54 +0530

    change in directory structure

--- core/models.py
+++ core/models.py
@@ -64,6 +64,7 @@ class Item(models.Model):
     slug = models.SlugField()
     description = models.TextField()
     image = models.ImageField()
+    image1= models.ImageField()
 
     def __str__(self):
         return self.title
--- core/views.py
+++ core/views.py
@@ -350,7 +350,7 @@ class PaymentView(View):
 class HomeView(ListView):
     model = Item
     paginate_by = 10
-    template_name = "site/index.html"
+    template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
